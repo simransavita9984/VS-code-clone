f826dbe72bd3d45730b21256f041e5123c7df306
simransavita9984 <simransavita11@gmail.com>
2025-06-24 18:01:33 +0530

    added js file

---
+++ script.js
@@ -0,0 +1,6 @@
+// Download button click functionality
+document.getElementById("downloadBtn").addEventListener("click", () => {
+  console.log("Download button clicked!");
+  alert("Your VS Code is being prepared for download...");
+});
+
