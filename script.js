386e917dda83aa127309ab8dda05d8f17f25fe63
simransavita9984 <simransavita11@gmail.com>
2025-06-24 18:08:09 +0530

    added js

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ document.getElementById("downloadBtn").addEventListener("click", () => {
   alert("Your VS Code is being prepared for download...");
 });
 
+// Search box enter event
+document.getElementById("searchInput").addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    alert(`You searched for: ${e.target.value}`);
+  }
+});
